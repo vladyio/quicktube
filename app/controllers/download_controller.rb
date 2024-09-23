0eb477e48c10627b36c9eb2b83ab93eecd88c1b6
Vladislav Andreev <andreev@vlad.social>
2024-09-23 21:07:21 +0300

    Clean up log messages

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -14,20 +14,16 @@ class DownloadController < ApplicationController
   end
 
   def show
-    Rails.logger.info "[DL] Downloading #{params[:filename]}"
-
     filename = params[:filename]
     file_path = Rails.root.join('public', 'dl', filename)
 
-    Rails.logger.info "[DL] Existance check: #{file_path}"
+    Rails.logger.info("[DL] Downloading: #{filename}, path: #{file_path}")
 
     if Pathname.new(file_path).exist?
-      Rails.logger.info "[DL] Existance check: OK"
-
+      Rails.logger.info("[DL] File found, sending for download")
       send_file file_path, disposition: 'attachment'
     else
-      Rails.logger.warn "[DL] Existance check: ERR"
-
+      Rails.logger.warn("[DL] File not found: #{filename}")
       render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false
     end
   end
