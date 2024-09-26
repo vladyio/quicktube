c1ebd09770a069104636b5fbe255e859d642be40
Vladislav Andreev <andreev@vlad.social>
2024-09-26 15:56:08 +0300

    Run formatter over config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
-require 'sidekiq/web'
+require "sidekiq/web"
 
 Rails.application.routes.draw do
   unless Rails.env.production?
-    mount Sidekiq::Web => '/sidekiq'
+    mount Sidekiq::Web => "/sidekiq"
   end
 
-  root 'home#index'
+  root "home#index"
 
-  get 'home/index'
-  get 'dl/*filename', to: 'download#show', as: :download_file, format: false
-  get 'download/index'
+  get "home/index"
+  get "dl/*filename", to: "download#show", as: :download_file, format: false
+  get "download/index"
 
 
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 end
