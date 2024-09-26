aa68f87a1a8aef76a5c335c91a44abe9b02bd883
Vladislav Andreev <andreev@vlad.social>
2024-09-26 15:57:20 +0300

    Use tagged logging

--- lib/dl_cleanup.rb
+++ lib/dl_cleanup.rb
@@ -1,10 +1,12 @@
-require 'fileutils'
+# frozen_string_literal: true
+
+require "fileutils"
 
 class DlCleanup
   def self.call
-    dl_dir = Rails.root.join('public', 'dl')
+    dl_dir = Rails.root.join("public", "dl")
     FileUtils.rm_rf(dl_dir)
 
-    Rails.logger.info "[#{__FILE__}] Removed all files from #{dl_dir}"
+    Rails.logger.tagged("Cleaner").info "[#{__FILE__}] Removed all files from #{dl_dir}"
   end
 end
