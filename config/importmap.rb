70a89ab77d010fb28f0edd430f47bcc30adee014
Vladislav Andreev <andreev@vlad.social>
2024-11-30 15:24:30 +0300

    Upgrade actioncable JS to 8.0

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.2.100
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.0.0
