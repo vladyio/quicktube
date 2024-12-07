08ad40fa0e425819e6c304ed912ab5c7f5cd078d
Vladislav Andreev <andreev@vlad.social>
2024-12-07 11:37:32 +0300

    Improve FilePathValidator

--- lib/file_path_validator.rb
+++ lib/file_path_validator.rb
@@ -4,6 +4,7 @@ class FilePathValidator
   attr_reader :file_path, :base_path
 
   DEFAULT_BASE_PATH = "public/dl"
+  ALLOWED_EXTENSION = ".mp3"
 
   def initialize(file_path)
     @file_path = file_path
@@ -11,6 +12,20 @@ class FilePathValidator
   end
 
   def valid?
-    file_path.to_s.start_with?(base_path.to_s) && Pathname.new(file_path).exist?
+    starts_with_base_path? && ends_with_allowed_extension? && file_exists?
+  end
+
+  private
+
+  def starts_with_base_path?
+    file_path.to_s.start_with?(base_path.to_s)
+  end
+
+  def ends_with_allowed_extension?
+    file_path.to_s.end_with?(".mp3")
+  end
+
+  def file_exists?
+    Pathname.new(file_path).exist?
   end
 end
