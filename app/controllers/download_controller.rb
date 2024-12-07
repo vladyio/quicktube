f74e38d48a903a9311948511a7402ae3d496c353
Vladislav Andreev <andreev@vlad.social>
2024-12-07 11:37:51 +0300

    Improve DownloadController downloading action

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DownloadController < ApplicationController
+  ALLOWED_FILES = [ "123" ]
   def index
     link = params[:link]
 
@@ -14,16 +15,18 @@ class DownloadController < ApplicationController
   end
 
   def show
-    filename = params[:filename]
-    file_path = Rails.root.join("public", "dl", filename).realdirpath
+    filename = File.basename(params[:filename])
+    file_path = Pathname.new(Rails.root.join("public", "dl", filename)).cleanpath
 
     Rails.logger.tagged("DL").info("Downloading: #{filename}, path: #{file_path}")
 
     if FilePathValidator.new(file_path).valid?
       Rails.logger.tagged("DL").info("File found, sending for download")
+
       send_file file_path, disposition: "attachment"
     else
       Rails.logger.tagged("DL").warn("File not found: #{filename}")
+
       render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false
     end
   end
